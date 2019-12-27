129c9b590fb4c0ea732101ff009010234421254b
Kévin Joya <kevin.joya@outlook.com>
2019-12-27 16:53:41 +0100

    Fetch db api

--- src/container/Feed/Feed.js
+++ src/container/Feed/Feed.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import FeedCard from "../../components/FeedCard/FeedCard";
 import FeedStyled from "./FeedStyled.style";
 
 function Feed() {
-  return(
-    <React.Fragment>
-      <FeedStyled className="feed-cards">
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-      </FeedStyled>
-    </React.Fragment>
-  )
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/comaStories")
+    .then(response => response.json())
+    .then(dataParsed => setData([...dataParsed] ))
+    .catch(error => alert(error))
+  }, [])
+
+  console.log(data);
+
+
+  if (data.length === 0) return "loading";
+
+  if (data.length !== 0) {
+    return data.map((card, index) => {
+      return(
+        <FeedStyled className="feed-cards" key={index}>
+          <FeedCard data={card} />
+        </FeedStyled>
+      )
+    })
+  }
 }
 
 export default Feed
