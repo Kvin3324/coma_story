ff4c24b6005753ca31da6222d2a86c94cdb1b60d
Kévin Joya <kevin.joya@outlook.com>
2020-01-03 17:44:45 +0100

    Check for inscription

--- src/components/Login/SpaceLog/SpaceLog.js
+++ src/components/Login/SpaceLog/SpaceLog.js
@@ -1,38 +1,53 @@
-import React, {useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import SpaceLogStyled from "./SpaceLogStyled.style";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
-function SpaceLog(props) {
-  const [data, setData] = useState([]);
+function SpaceLog() {
+  const [data, setData] = useState({
+    users: [],
+    redirection: false,
+    showNotif: false
+  });
   const emailValue = useRef(null);
   const pswValue = useRef(null);
 
   function checkLogs() {
-    fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        mail: emailValue.current.value,
-        paswword: pswValue.current.value
-      })
-    })
-    .then(response => response.json())
-    .then(dataParsed => {
-      fetch("http://localhost:3000/db")
+    fetch("http://localhost:3000/users")
       .then(response => response.json())
-      .then(parsedData => setData([...parsedData.users]))
-    })
+      .then(dataParsed => {
+        if (!dataParsed.find(user => user.mail === emailValue.current.value)) {
+          fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+              mail: emailValue.current.value,
+              password: pswValue.current.value
+            })
+          })
+            .then(response => response.json())
+            .then(parsedData => setData({
+              users: dataParsed,
+              redirection: true,
+              shwoNotif: false
+            })
+            )
+        } else {
+          const newState = { ...data };
+
+          newState.showNotif = true;
+          setData(newState)
+        }
+      })
   }
 
   console.log(data);
 
-
-  return(
+  return (
     <React.Fragment>
       <SpaceLogStyled className="space--log mt-5">
-        <h1 className="mb-5">Se connecter:</h1>
+        <h1 className="mb-5">Inscription:</h1>
         <div className="space--log--mail">
           <label htmlFor="mail" className="mr-4" >Email:</label>
           <input type="email" id="mail" className="form-group" placeholder="name@example.com" size="30" ref={emailValue} required />
@@ -42,9 +57,21 @@ function SpaceLog(props) {
           <input type="password" id="password" className="form-group" name="password" ref={pswValue} required />
         </div>
         <div className="space--log--submit">
-          <Link to={`${props.match.path = "/"}`}  >
-            <button className="btn btn-primary" onClick={checkLogs} >Sign in</button>
-          </Link>
+          <button className="btn btn-primary" onClick={checkLogs} >Sign in</button>
+          {
+            function () {
+              if (data.showNotif === true) {
+                return (
+                  <div className="alert alert-danger mt-4" role="alert">
+                    <p>Un compte existe déjà, veuillez utiliser une adresse mail différente.</p>
+                  </div>
+                )
+              }
+              if (data.redirection === true) {
+                return <Redirect to="/" />
+              }
+            }()
+          }
         </div>
       </SpaceLogStyled>
     </React.Fragment>
