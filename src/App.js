44267b02db3f87ca521d68f7b6cd806509098feb
Kévin Joya <kevin.joya@outlook.com>
2020-01-06 17:38:44 +0100

    Added new route for connection space

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Feed from './container/Feed/Feed';
 import { Route, BrowserRouter } from "react-router-dom";
 import SpaceLog from './components/Login/SpaceLog/SpaceLog';
+import SpaceConnection from './components/Login/SpaceConnection/SpaceConnection';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
       <Header />
       <Route exact path="/logIn" component={SpaceLog} />
+      <Route exact path="/connection" component={SpaceConnection} />
         <div className="container">
           <Route exact path="/" component={About}  ></Route>
           <Route exact path="/" component={Feed}  ></Route>
