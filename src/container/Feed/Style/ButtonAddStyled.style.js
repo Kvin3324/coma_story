6a697fd12d69e2167de699b7ffe60b7139a065c3
Kévin Joya <kevin.joya@outlook.com>
2020-01-06 16:16:29 +0100

    Added style for btn

---
+++ src/container/Feed/Style/ButtonAddStyled.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ButtonAddStyled = styled.div`
+  position: absolute;
+  top: 66px;
+  button {
+    border: none;
+    background-color: white;
+    color: #EF271B;
+      :hover {
+        background-color: white;
+        color: #EF271B;
+      }
+  }
+`
+
+export default ButtonAddStyled
