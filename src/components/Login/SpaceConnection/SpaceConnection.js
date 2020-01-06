0e62db4d68acf5be846adfd1c20b11218b0e22cb
Kévin Joya <kevin.joya@outlook.com>
2020-01-06 17:39:54 +0100

    Created compo for co space

---
+++ src/components/Login/SpaceConnection/SpaceConnection.js
@@ -0,0 +1,57 @@
+import React, {useState, useRef, useEffect} from "react";
+import SpaceLogStyled from "../SpaceLog/SpaceLogStyled.style";
+import { Redirect } from "react-router-dom";
+
+function SpaceConnection() {
+  const [data, setData] = useState({
+    users: [],
+    redirectionConnection: false
+  })
+  const inputMail = useRef(null);
+  const inputPsw = useRef(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+    .then(response => response.json())
+    .then(dataParsed => setData({
+      users: dataParsed,
+      redirectionConnection: false
+    }))
+  }, [])
+
+  function checkAccount() {
+    if (data.users.find(user => user.mail === inputMail.current.value && user.password === inputPsw.current.value)) {
+      const newState = {...data};
+      newState.redirectionConnection = true;
+      setData(newState);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <SpaceLogStyled className="space--log mt-5">
+        <h1 className="mb-5">Connexion:</h1>
+        <div className="space--log--mail">
+          <label htmlFor="mail" className="mr-4">Email:</label>
+          <input type="email" id="mail" className="form-group" placeholder="name@example.com" size="30" ref={inputMail} required />
+        </div>
+        <div className="space--log--password">
+          <label htmlFor="mail" className="mr-4">Mot de passe:</label>
+          <input type="password" id="password" className="form-group" name="password" ref={inputPsw} required />
+        </div>
+        <div className="space--log--submit">
+          <button className="btn btn-primary" onClick={checkAccount}>Connexion</button>
+          {
+            function () {
+              if (data.redirectionConnection === true) {
+                return <Redirect to="/" />
+              }
+            }()
+          }
+        </div>
+      </SpaceLogStyled>
+    </React.Fragment>
+  )
+}
+
+export default SpaceConnection
