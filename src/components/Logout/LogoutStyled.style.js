e7332493c554cf84f88dce1930f8260b2a10303f
Kévin Joya <kevin.joya@outlook.com>
2020-01-08 15:32:23 +0100

    Created compo style logout

---
+++ src/components/Logout/LogoutStyled.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LogoutStyled = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin-right: 15%;
+    button {
+      background: white;
+      border: none;
+      color: #EF271B;
+      margin-bottom: 10px;
+    }
+`
+
+export default LogoutStyled
