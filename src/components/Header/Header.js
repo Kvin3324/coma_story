072a4f5de8073ea03c1df6ce2aaa64b8c29f5cc0
Kévin Joya <kevin.joya@outlook.com>
2020-01-08 12:28:25 +0100

    Added store to render space log

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import HeaderStyled from "./HeaderStyled.style";
 import Login from "../Login/Login/Login";
+import store from "../../redux/store";
+import Logout from "../Logout/Logout";
+
 
 function Header() {
+  const [data, setData] = useState(store.getState());
+  store.subscribe(() => setData(store.getState()));
+
   return(
     <header>
       <HeaderStyled className="header--title">
         <h1 className="pt-4 pl-5">#COMA</h1>
-      <Login />
+        {
+          function () {
+            if (store.getState() === false) {
+              return <Login />
+            }
+            if (store.getState() === true) {
+              return <Logout />
+            }
+          }()
+        }
       </HeaderStyled>
     </header>
   )
