9c2d6118729448635b9f6a15efca817bf81d1330
Kévin Joya <kevin.joya@outlook.com>
2020-01-08 15:31:44 +0100

    Added header fixed

--- src/components/Header/HeaderStyled.style.js
+++ src/components/Header/HeaderStyled.style.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 const HeaderStyled = styled.div`
-background-color: #EF271B;
-  h1 {
-    text-align: left;
-    color: #FBEEEA;
-  }
+  background-color: #EF271B;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+    h1 {
+      text-align: left;
+      color: #FBEEEA;
+    }
 `
 
 export default HeaderStyled
