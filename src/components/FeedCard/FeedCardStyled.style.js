95ad5b9afe6041b2e68320567734cc03d8549fef
Kévin Joya <kevin.joya@outlook.com>
2020-01-08 15:31:34 +0100

    Added some style on cards

--- src/components/FeedCard/FeedCardStyled.style.js
+++ src/components/FeedCard/FeedCardStyled.style.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
 const FeedCardStyled = styled.div`
+  border-radius: 2%;
+  box-shadow: 1px 1px 2px grey;
   .card-infos {
+    h2 {
+      font-size: 20px;
+      font-weight: 700;
+    }
     h2, p {
+      padding: 10px 25px;
       text-align: left;
     }
   }
