d850114f7d859aaadae4f1b7603c723ff1dc3099
Kévin Joya <kevin.joya@outlook.com>
2020-01-09 21:08:06 +0100

    Added condition to edit if user co

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import FeedCardStyled from "./FeedCardStyled.style";
+import store from "../../redux/store";
 
 function FeedCard(props) {
+  const [user, setUser] =  useState(store.getState())
+
   return(
     <FeedCardStyled className="card mt-4">
+      {
+        user.userId === props.data.authorId && <i className="fas fa-pencil-alt" onClick={props.editStory}></i>
+      }
       <div className="card-infos">
         <h2>{props.data.title}</h2>
         <p>{props.data.content}</p>
