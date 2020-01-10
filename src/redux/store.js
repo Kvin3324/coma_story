e6e6c8fd2b41eafe6208953245a9b88f5563cd0f
Kévin Joya <kevin.joya@outlook.com>
2020-01-10 18:05:56 +0100

    Added store property

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,19 @@
 import {createStore} from "redux";
 
 const appState = {
-  userId: null
+  userId: null,
+  editStory: false
 };
 
 function connection(state = appState, action) {
   const newState = {...state};
 
   if (action.type === "USER_CONNECTED") {
-    // newState.isConnected = true;
     newState.userId = action.userId;
+    return newState;
+  }
+  if (action.type === "EDIT_STORY") {
+    newState.editStory = true;
     return newState;
   }
   return state;
